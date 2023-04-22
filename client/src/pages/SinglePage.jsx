fbbeb72d9bde15ed08242eb791254d40ec046db1
nero-gh <neroaboagye@gmail.com>
2023-04-22 22:50:31 +0000

    added delete api

--- client/src/pages/SinglePage.jsx
+++ client/src/pages/SinglePage.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import moment from "moment";
+import DOMPurify from "dompurify";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import Menu from "../components/Menu.jsx";
 
 const SinglePage = () => {
+  const [post, setPost] = useState({});
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
+
+  //getting single post
+  const postId = location.pathname.split("/")[2];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
+
   return (
     <div className="single">
-      {/* <div className="content">
+      <div className="content">
         <img src={`../upload/${post?.img}`} alt="" />
         <div className="user">
           {post.userImg && <img src={post.userImg} alt="" />}
@@ -27,7 +70,7 @@ const SinglePage = () => {
           }}
         ></p>{" "}
       </div>
-      <Menu cat={post.cat} /> */}
+      <Menu cat={post.cat} />
     </div>
   );
 };
